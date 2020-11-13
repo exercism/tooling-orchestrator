5e22176b7486151a1e0198e8e16b49479738c1e3
Jeremy Walker <jez.walker@gmail.com>
2020-11-13 01:14:55 +0000

    Remove S3 and use new metadata key

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -33,11 +33,5 @@ module Minitest
         key: { id: job_id }
       ).item
     end
-
-    def download_s3_file(bucket, key)
-      client = ExercismConfig::SetupS3Client.()
-      client.get_object(bucket: bucket,
-                        key: key).body.read
-    end
   end
 end
