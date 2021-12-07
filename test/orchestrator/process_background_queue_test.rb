44478778083371aaa51aa7787bef7df3604a2f5c
Jeremy Walker <jez.walker@gmail.com>
2021-12-07 16:36:39 +0000

    Up max queue size to 20 (#35)

--- test/orchestrator/process_background_queue_test.rb
+++ test/orchestrator/process_background_queue_test.rb
@@ -2,45 +2,47 @@ require 'test_helper'
 
 module Orchestrator
   class ProcessBackgroundQueueTest < Minitest::Test
+    MAX_QUEUE_LENGTH = ProcessBackgroundQueue::MAX_QUEUE_LENGTH
+
     def test_is_fine_with_nothing_in_background_queue
       cmd = ProcessBackgroundQueue.new
       cmd.expects(:loop).yields
       cmd.()
     end
 
-    def test_moves_up_to_10_things_over
-      15.times do
+    def test_moves_up_to_MAX_QUEUE_LENGTH_things_over # rubocop:disable Naming/MethodName
+      (MAX_QUEUE_LENGTH + 5).times do
         Exercism::ToolingJob.create!(SecureRandom.uuid, :test_runner, :ruby, "two-fer", run_in_background: true)
       end
 
       assert_equal 0, redis.llen(Exercism::ToolingJob.key_for_queued)
-      assert_equal 15, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
+      assert_equal MAX_QUEUE_LENGTH + 5, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
 
       cmd = ProcessBackgroundQueue.new
       cmd.expects(:loop).yields
       cmd.()
 
-      assert_equal 10, redis.llen(Exercism::ToolingJob.key_for_queued)
+      assert_equal MAX_QUEUE_LENGTH, redis.llen(Exercism::ToolingJob.key_for_queued)
       assert_equal 5, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
     end
 
-    def test_sets_max_queue_size_as_10
-      15.times do
+    def test_sets_max_queue_size_as_MAX_QUEUE_LENGTH # rubocop:disable Naming/MethodName
+      (MAX_QUEUE_LENGTH + 5).times do
         Exercism::ToolingJob.create!(SecureRandom.uuid, :test_runner, :ruby, "two-fer", run_in_background: true)
       end
-      6.times do
+      (MAX_QUEUE_LENGTH - 4).times do
         Exercism::ToolingJob.create!(SecureRandom.uuid, :test_runner, :ruby, "two-fer", run_in_background: false)
       end
 
-      assert_equal 6, redis.llen(Exercism::ToolingJob.key_for_queued)
-      assert_equal 15, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
+      assert_equal MAX_QUEUE_LENGTH - 4, redis.llen(Exercism::ToolingJob.key_for_queued)
+      assert_equal MAX_QUEUE_LENGTH + 5, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
 
       cmd = ProcessBackgroundQueue.new
       cmd.expects(:loop).yields
       cmd.()
 
-      assert_equal 10, redis.llen(Exercism::ToolingJob.key_for_queued)
-      assert_equal 11, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
+      assert_equal MAX_QUEUE_LENGTH, redis.llen(Exercism::ToolingJob.key_for_queued)
+      assert_equal MAX_QUEUE_LENGTH + 1, redis.llen(Exercism::ToolingJob.key_for_queued_for_background_processing)
     end
 
     def redis
